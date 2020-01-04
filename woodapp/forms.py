8cd9755675afa9c986c3807ee2880c19329348e5
Rack <rack1738@gmail.com>
2020-01-04 17:16:40 +0545

    forms complited

--- woodapp/forms.py
+++ woodapp/forms.py
@@ -8,7 +8,17 @@ class OrderForm(forms.ModelForm):
         model = Order
         # fields = '__all__'
         fields = ['street', 'mobile']
+        widgets = {
+            'street': forms.TextInput(attrs={
+                'class': 'username',
+                'placeholder': 'Street'
+            }),
 
+            'mobile': forms.TextInput(attrs={
+                'class': 'password',
+                'placeholder': 'Mobile No'
+            })
+        }
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -26,9 +36,16 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'autocomplete': 'off'
+        'class': 'username',
+        'autocomplete': 'off',
+        'placeholder': 'Enter Username'
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'autocomplete': 'new-password'
+        'class': 'password',
+        'autocomplete': 'new-password',
+        'placeholder': 'Enter Password'
     }))
-    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'c_password',
+        'placeholder': 'Confirm Password'
+    }))
